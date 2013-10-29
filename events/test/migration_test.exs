85e3d6af6735bae84c8621a4a62ad3c07ae2c872
ap4y <lod@pisem.net>
2013-10-29 14:40:54 +1300

    Added additional tests for migration

--- events/test/migration_test.exs
+++ events/test/migration_test.exs
@@ -5,7 +5,19 @@ defmodule MigrationTest do
 
   @document {
     :app_id, {"foo"},
+    :device_id, {"bar"},
+    :notification_id, {"baz"},
     :code, 1000,
+    :date, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{10,15,23}}),
+    :value, 100,
+    :created_at, Events.DateUtils.datetime_to_unixtime({{2012,12,05},{0,0,0}}),
+  }
+
+  @document2 {
+    :app_id, {"foo"},
+    :device_id, {"bar"},
+    :notification_id, {"baz"},
+    :code, 1100,
     :date, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{10,15,23}}),
     :value, 100,
     :created_at, Events.DateUtils.datetime_to_unixtime({{2012,12,05},{0,0,0}}),
@@ -15,23 +27,83 @@ defmodule MigrationTest do
     [ _id: {"foo"}, created_at: {1349, 665482, 0} ]
   ]
 
-  test "it correctly iterates through cursor" do
+  setup do
     :meck.new(:mongo)
+  end
+
+  teardown do
+    :meck.unload(:mongo)
+  end
+
+  test "it correctly iterates through cursor. app session mock" do
     :meck.expect(:mongo, :next, fn(_) -> {@document} end)
     :meck.expect(:mongo, :close_cursor, fn(cursor) ->
       assert cursor == "foo"
     end)
     :meck.expect(:mongo, :repsert, fn(collection, find, update) ->
-      assert collection == :"app_events.daily"
-      assert find == {
-        :_id, {:p, {"foo"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
-      }
-      assert update == {
-        "$inc", {"h.10.s", 1},
-        "$inc", {"h.10.t", 100},
-        "$inc", {"m.10.15.s", 1},
-        "$inc", {"m.10.15.t", 100}
-      }
+      case collection do
+        :"app_events.daily" ->
+          assert find == {
+            :_id, {:p, {"foo"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
+            }
+          assert update == {
+            "$inc", {"h.10.s", 1},
+            "$inc", {"h.10.t", 100},
+            "$inc", {"m.10.15.s", 1},
+            "$inc", {"m.10.15.t", 100}
+            }
+        :"device_events.daily" ->
+          assert find == {
+            :_id, {:p, {"bar"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
+            }
+          assert update == {
+            "$inc", {"h.10.s", 1},
+            "$inc", {"h.10.t", 100},
+            "$inc", {"m.10.15.s", 1},
+            "$inc", {"m.10.15.t", 100}
+            }
+        _ -> flunk "Wrong collection"
+      end
+    end)
+
+    process_cursor("foo", @apps, 2)
+
+    assert :meck.validate(:mongo) == true
+  end
+
+  test "it correctly iterates through cursor. app open mock" do
+    :meck.expect(:mongo, :next, fn(_) -> {@document2} end)
+    :meck.expect(:mongo, :close_cursor, fn(cursor) ->
+      assert cursor == "foo"
+    end)
+    :meck.expect(:mongo, :repsert, fn(collection, find, update) ->
+      case collection do
+        :"app_events.daily" ->
+          assert find == {
+            :_id, {:p, {"foo"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
+            }
+          assert update == {
+            "$inc", {"h.10.c", 1},
+            "$inc", {"m.10.15.c", 1}
+            }
+        :"device_events.daily" ->
+          assert find == {
+            :_id, {:p, {"bar"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
+            }
+          assert update == {
+            "$inc", {"h.10.c", 1},
+            "$inc", {"m.10.15.c", 1}
+            }
+        :"notification_events.daily" ->
+          assert find == {
+            :_id, {:p, {"baz"}, :d, Events.DateUtils.datetime_to_unixtime({{2012,11,04},{0,0,0}}) }
+            }
+          assert update == {
+            "$inc", {"h.10.c", 1},
+            "$inc", {"m.10.15.c", 1}
+            }
+        _ -> flunk "Wrong collection"
+      end
     end)
 
     process_cursor("foo", @apps, 2)
